c23ed3aced1d023e24c05fa1486e9a3c453db214
Anna Efimova <annaryabko@gmail.com>
2022-09-25 16:34:43 +0200

    up

--- backend/app.js
+++ backend/app.js
@@ -24,12 +24,22 @@ const allowedCors = [
   'http://anna.nomorepartiesxyz.ru',
   'http://localhost:3000',
 ];
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 app.use((req, res, next) => {
+  const { method } = req;
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  const requestHeaders = req.headers['access-control-request-headers'];
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.end();
+    return;
   }
 
   next();
