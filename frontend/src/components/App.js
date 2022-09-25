32733c64f1533ea08d1fd7c78f88021393aede7e
Anna Efimova <annaryabko@gmail.com>
2022-09-25 19:12:49 +0200

    likes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -70,7 +70,7 @@ function App({match, location, history}) {
   }, []);
 
   function handleCardLike(card) {
-    const isLiked = card.likes.some((i) => i._id === currentUser._id);
+    const isLiked = card.likes.some((i) => i === currentUser._id);
 
     api
       .changeLikeCardStatus(card._id, isLiked)
--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -16,6 +16,7 @@ function Card({ card, onCardClick, onCardLike, onCardDelete }) {
 
   const currentUser = React.useContext(CurrentUserContext);
   // Определяем, являемся ли мы владельцем текущей карточки
+  console.log(card, currentUser);
   const isOwn = card.owner._id === currentUser._id; //true or false
 
   // Создаём переменную, которую после зададим в `className` для кнопки удаления
@@ -24,7 +25,7 @@ function Card({ card, onCardClick, onCardLike, onCardDelete }) {
   }`;
 
   // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
-  const isLiked = card.likes.some((i) => i._id === currentUser._id);
+  const isLiked = card.likes.some((i) => i === currentUser._id);
 
   // Создаём переменную, которую после зададим в `className` для кнопки лайка
   const cardLikeButtonClassName = `${
